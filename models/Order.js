c3085761690b81ea79be47ab632425111960f1d3
eh4git <ehirsch760@gmail.com>
2022-11-07 00:12:55 -0800

    mongoose models, mongodb atlas connected

---
+++ models/Order.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+// if (!mongoose.models.Order) {
+const orderSchema = new Schema({
+  purchaseDate: {
+    type: Date,
+    default: Date.now,
+  },
+  products: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Product",
+    },
+  ],
+});
+// console.log(mongoose);
+
+const Order = mongoose.models.Order || mongoose.model("Order", orderSchema);
+// }
+
+module.exports = Order;
